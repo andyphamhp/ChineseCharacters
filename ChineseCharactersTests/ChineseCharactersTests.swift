dcb1a9829bc4bb44e9addcd61bc6689eb1eed0e5
Andy Pham <andyphamhp9x@gmail.com>
2025-03-30 18:43:51 +0700

    Initial Commit

---
+++ ChineseCharactersTests/ChineseCharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseCharactersTests.swift
+//  ChineseCharactersTests
+//
+//  Created by Andy Pham on 30/3/25.
+//
+
+import Testing
+@testable import ChineseCharacters
+
+struct ChineseCharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
